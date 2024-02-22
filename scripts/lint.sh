744a789d9de68187ab603473aaec3afd5a7109d5
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2024-02-22 18:13:45 +0100

    Convert pylint config to toml

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,10 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+
+SCRIPT_DIR="$(cd "$(dirname "$(realpath "$0")")" && pwd -P)"
+PROJECT_ROOT="$(cd "$SCRIPT_DIR/.." && pwd -P)"
+cd "$PROJECT_ROOT"
 
 PYTHON_MOD='docker_rate_limit'
 
 mypy $PYTHON_MOD
 
-pylint $PYTHON_MOD
+pylint --rcfile .pylintrc.toml $PYTHON_MOD
 
 pydoclint $PYTHON_MOD
 
