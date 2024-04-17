3ce4c291b683f78fb3052177a09fd39014a3567d
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2024-04-17 18:39:07 +0200

    Add error handling and support for multiple formats

--- docker_rate_limit/http_server.py
+++ docker_rate_limit/http_server.py
@@ -3,10 +3,13 @@
 from http.server import HTTPServer
 from http.server import BaseHTTPRequestHandler
 import sys
+from urllib.parse import parse_qs
+from urllib.parse import urlparse
 
 from typing import Any
 
 from .docker_hub import get_rate_limit
+from .output_format import RateLimitOutputFormat
 
 class DockerRateLimitHTTPServer(HTTPServer):
     """
@@ -37,20 +40,93 @@ class DockerRateLimitRequestHandler(BaseHTTPRequestHandler):
 
         super().__init__(*args, **kwargs)
 
+    def send_http_error_message(self, code: int, message: str) -> None:
+        """
+        Send HTTP error message
+
+        :param code: HTTP error code to send
+        :param message: Plaintext message to include in body of response
+        """
+        # End message with newline character
+        if len(message) > 0 and message[-1] != '\n':
+            message += '\n'
+
+        self.protocol_version = 'HTTP/1.1'
+        self.send_response(code)
+        self.send_header('Content-Length', str(len(message)))
+        self.end_headers()
+        self.wfile.write(bytes(message, 'utf-8'))
+
+    def send_rate_limit_response(self, output_format: RateLimitOutputFormat) -> None:
+        """
+        Send HTTP response with docker rate limit in specified format.
+
+        :param output_format: Format in which to respond
+        """
+
+        # Get rate limit
+        rate_limit = get_rate_limit()
+        payload = rate_limit.to_output_format(output_format)
+
+        # End payload with newline character
+        if len(payload) > 0 and payload[-1] != '\n':
+            payload += '\n'
+
+        self.protocol_version = 'HTTP/1.1'
+        self.send_response(200)
+        self.send_header('Content-Length', str(len(payload)))
+
+        # Set Content-Type header accordingly
+        if output_format is RateLimitOutputFormat.JSON:
+            self.send_header('Content-Type', 'application/json')
+        if output_format is RateLimitOutputFormat.YAML:
+            self.send_header('Content-Type', 'application/yaml')
+
+        self.end_headers()
+        self.wfile.write(bytes(payload, 'utf-8'))
+
     # pylint: disable=invalid-name
     def do_GET(self) -> None:
         """
         Handle GET request to HTTP server
         """
 
-        # Get rate limit
-        rate_limit = get_rate_limit()
-        payload = rate_limit.to_json()
+        # Parse request
+        urltuple = urlparse(self.path)
+        path = urltuple.path
+        arguments = parse_qs(urltuple.query)
 
-        # Send response
-        self.protocol_version = 'HTTP/1.1'
-        self.send_response(200)
-        self.send_header('Content-Length', str(len(payload)))
-        self.send_header('Content-Type', 'application/json')
-        self.end_headers()
-        self.wfile.write(bytes(payload, 'utf-8'))
+        # Return HTTP-404 for every location but /
+        if path != '/':
+            self.protocol_version = 'HTTP/1.1'
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        # Check for unexpected arguments
+        supported_args = {'format'}
+        for arg in arguments:
+            if arg not in supported_args:
+                message = f'Error: Unknown query string "{arg}"'
+                self.send_http_error_message(400, message)
+                return
+
+        # Extract format from request
+        # Default format is JSON
+        format_enum = RateLimitOutputFormat.JSON
+        if 'format' in arguments:
+            if len(arguments['format']) > 1:
+                message = 'Error: Expected exactly one value for parameter "format"'
+                self.send_http_error_message(400, message)
+                return
+
+            format_str = arguments['format'][0].lower()
+            try:
+                format_enum = RateLimitOutputFormat(format_str)
+            except ValueError:
+                message = f'Error: Unsupported format \"{format_str}\"'
+                self.send_http_error_message(400, message)
+                return
+
+        self.send_rate_limit_response(format_enum)
+        return
