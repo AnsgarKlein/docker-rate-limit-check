c1b234c1a573feba82df8b4a6192143978f821dc
Ansgar Klein <AnsgarKlein@users.noreply.github.com>
2025-04-01 21:31:40 +0200

    Dont highlight syntax of output in prometheus format
    rich.print() does fine syntax highlighting for yaml and json but not for
    prometheus metrics.

--- docker_rate_limit_check/__main__.py
+++ docker_rate_limit_check/__main__.py
@@ -58,7 +58,12 @@ def query(
 
     # Output in correct format
     output = rate_limit.to_output_format(output_format)
-    rich.print(output)
+
+    # Output with or without syntax highlight (depending on format)
+    if output_format in [RateLimitOutputFormat.JSON, RateLimitOutputFormat.YAML]:
+        rich.print(output)
+    else:
+        print(output)
 
 @app.command(help='''
 Run HTTP server that responds with rate limit''')
